1cf72ae46ca72ca5462344f72916a4d7b15cd14e
Trudy <trudy@vpnhood.com>
2025-03-01 12:41:44 -0800

    Init

---
+++ VpnHood.AppLib.Assets.IpLocations/IpLocationDb.cs
@@ -0,0 +1,7 @@
+﻿namespace VpnHood.AppLib.Assets.IpLocations;
+
+public static class IpLocationDb
+{
+    private static readonly Lazy<byte[]> IpLocationZipDataLazy = new(() => Resources.IpLocationZipData);
+    public static byte[] IpLocationZipData => IpLocationZipDataLazy.Value;
+}
