dd51e70458dd31edd262737d97feaef3bf9e5ee3
Trudy <trudy@vpnhood.com>
2025-10-28 20:50:19 -0700

    Automate Git operations in _publish.ps1
    Added `$SoultionDir` and `$ProjectDir` variables to define paths based on the script's location. Introduced `$GitDirArgs` to centralize Git directory and work-tree arguments. Automated Git commit, pull, and push commands to streamline version control directly within the script.

---
+++ VpnHood.AppLib.Assets.Ip2LocationLite/_publish.ps1
@@ -0,0 +1,9 @@
+﻿$SoultionDir = Split-Path -Parent $PSScriptRoot
+$ProjectDir = $PSScriptRoot
+
+# Common git directory/work-tree arguments
+$GitDirArgs = @('--git-dir', "$SoultionDir/.git", '--work-tree', $SoultionDir)
+
+git $GitDirArgs commit -a -m Publish
+git $GitDirArgs pull
+git $GitDirArgs push
